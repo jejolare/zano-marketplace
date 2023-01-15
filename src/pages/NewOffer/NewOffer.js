8a264fb20799ab5983d69eb6f3fbf22090d621b1
andrewprog97 <andrewprog97@gmail.com>
2023-01-16 01:56:48 +0300

    edit offer page styles

--- src/pages/NewOffer/NewOffer.js
+++ src/pages/NewOffer/NewOffer.js
@@ -1,7 +1,9 @@
+import "./newOffer.scss";
 import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 import Form from "../../components/Form/Form";
 import Input from "../../components/Input/Input";
+import Card from "../../components/Card/Card"
 import { useNavigate } from "react-router-dom";
 
 function NewOffer() {
@@ -10,33 +12,36 @@ function NewOffer() {
         <div className="offer-page">
             <Header Button={<Button onMouseUp={() => navigate("/")}>Go Back</Button>}/>
             <main>
-                <Form>
-                    <h3>Create New Offer</h3>
-                    <p>Title</p>
-                    <Input className="ui__input"/>
-                    <span>Maximum length is 30 characters</span>
-                    <p>Category</p>
-                    <Input className="ui__input"/>
-                    <p>Description</p>
-                    <Input className="ui__input"/>
-                    <div className="ui__form__wrapper">
-                        <div className="ui__input__wrapper">
-                            <p>Price</p>
-                            <Input className="ui__input ui__input__price"/>
-                            <div>ZANO</div>
+                <div className="ui__main__wrapper">
+                    <Form>
+                        <h3>Create New Offer</h3>
+                        <p>Title</p>
+                        <Input className="ui__input"/>
+                        <span>Maximum length is 30 characters</span>
+                        <p>Category</p>
+                        <Input className="ui__input"/>
+                        <p>Description</p>
+                        <Input className="ui__input"/>
+                        <div className="ui__form__wrapper">
+                            <div className="ui__input__wrapper">
+                                <p>Price</p>
+                                <Input className="ui__input ui__input__price"/>
+                                <div>ZANO</div>
+                            </div>
+                            <div>
+                                <p>Contact details</p>
+                                <Input className="ui__input ui__input__author"/>
+                            </div>
                         </div>
-                        <div>
-                            <p>Contact details</p>
-                            <Input className="ui__input ui__input__author"/>
-                        </div>
-                    </div>
-                    <p>Image link</p>
-                    <Input className="ui__input ui__input__image"/>
-                    <p>Comments</p>
-                    <Input className="ui__input ui__input__comments"/>
-                    <button className="ui__submit-btn">Submit</button>
-                    <span className="ui__warn">You'll Be Prompted To Confirm Your Submission With Zano Wallet</span>
-                </Form>
+                        <p>Image link</p>
+                        <Input className="ui__input ui__input__image"/>
+                        <p>Comments</p>
+                        <Input className="ui__input ui__input__comments"/>
+                        <button className="ui__submit-btn">Submit</button>
+                        <span className="ui__warn">You'll Be Prompted To Confirm Your Submission With Zano Wallet</span>
+                    </Form>
+                    <Card />
+                </div>
             </main>
         </div>
     );
